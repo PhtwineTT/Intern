1e8044f40d37af53d286f56d97a69ab2d4b522ae
PhtwineTT <ndttu029@gmail.com>
2026-07-22 09:02:47 +0700

    Delete 02_Module directory

--- 02_Module/WebApplication2/DTOs/RewardDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Module.DTOs
-{
-    public class RewardDto
-    {
-        public string ItemName { get; set; } = string.Empty;
-        public int StockQuantity { get; set; }
-        public int PointsRequired { get; set; }
-    }
-}
